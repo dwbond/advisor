f053fe36c17655d1d94fd04554e977c84c7ab4a4
Daniel Warren Bond <dbond2@gmu.edu>
2013-12-04 02:03:06 -0500

    perfunctory urls setup

--- advisor/settings/urls.py
+++ advisor/settings/urls.py
@@ -3,10 +3,28 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns('trajectories.views',
     # Examples:
     # url(r'^$', 'advisor.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    # homepage
+    url(r'^$', 'index', name = 'homepage'),
+
+    # slug is the course
+    # course
+    url(r'^(?P<slug>[^\.]+)/$', 'course', name = 'course'),
+
+    # student's page
+    url(r'^(?P<slug>[^\.]+)/$', 'student', name = 'student'),
+
+    # slug is the student's
+    # comparison page
+    url(r'^(?P<slug>[^\.]+)/compare/$', 'compare', name = 'compare'),
+
+    # about page
+    url(r'^$', 'about', name = 'about'),
+
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/doc/' include('django.contrib.admindocs.urls')),
 )
