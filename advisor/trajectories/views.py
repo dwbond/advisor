6aa0fe42fe78e8cc093cecc39dea5ab315a56bb1
Daniel Warren Bond <dbond2@gmu.edu>
2013-12-07 18:59:20 -0500

    moved some functions from ideas text to views

--- advisor/trajectories/views.py
+++ advisor/trajectories/views.py
@@ -3,8 +3,40 @@ from trajectories.models import Course, Trajectory, Student
 
 # other functions
 
-# the hard function
+def remainingReqCourses(requiredCourses, coursesTaken):
+    """ returns remaining courses for program """
+    remainingReqCourses = []
+    for course in requiredCourses:
+        if course not in coursesTaken:
+            remainingReqCourses.append(course)
 
+def allPrereqCoreq(course, remainingReqCourses):
+    """ returns required courses that have course as a prereq or coreq """
+    allPrereqCoreq = []
+    for requiredCourse in remainingReqCourses:
+        for prereq in course.prereqs:
+            if prereq is requiredCourse:
+                allPrereqCoreq.append(prereq)
+        for coreq in course.coreqs:
+            if coreq is requiredCourse:
+                allPrereqCoreq.append(coreq)
+    return allPreqCoreq
+
+def nextCourses(coursesTaken, remainingReqCourses):
+     """ returns the courses student can take given what's been taken """
+     nextCourses = []
+     for course in remainingReqCourses:
+         if course.prereqs not in coursesTaken:
+             nextCourses.append(course)
+         # you have to take the prereqs for something first
+         elif course.coreq not in coursesTaken:
+             nextCourses.append(course)
+         else:
+             possiblity = allPrereqCoreq(course, remainingReqCourses)
+             append nextCourses.append(possiblity)
+     return nextCourses
+
+# how does one deal with "you have to take the coreq at the same time?"
 
 
 # page render functions
