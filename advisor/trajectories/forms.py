25b961d6f0b0f7ebb410998ad473245af312f57e
Daniel Warren Bond <dbond2@gmu.edu>
2014-01-30 20:33:13 -0500

    added search to the forms

--- advisor/trajectories/forms.py
+++ advisor/trajectories/forms.py
@@ -4,6 +4,19 @@ from django.core.exceptions import ValidationError
 from django.forms import ModelForm, Select
 from trajectories.models import Trajectory
 
+from haystack.forms import SearchForm
+
+class StyledSeachForm( SearchForm ):
+    q = forms.CharField(
+        required = False,
+	label = 'Search',
+	widget = forms.TextInput(attrs={
+	    'class' : 'form-control',
+	    'placeholder' : 'Search',
+	    'autofocus' : 'autofocus',
+        }),
+    )
+
 class NewTrajectoryForm( ModelForm ):
     # def __init__(self, *args, **kwargs):
 
